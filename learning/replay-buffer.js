e4519b9ff4876f21b2a2644073ba26dacaf8304d
Ashwin Dharne <ashwindharne@dm.ai>
2019-03-04 11:20:50 -0800

    initial commit for ddpg

---
+++ learning/replay-buffer.js
@@ -0,0 +1,42 @@
+class ReplayBuffer {
+
+    /**
+     *
+     * @param max_size
+     */
+    constructor(max_size){
+        this.max_size = max_size;
+        this.count = 0;
+        this.buffer = [];
+    }
+
+    _insert(state, action, new_state, reward){
+        if()
+        this.buffer.append({
+           state: state,
+           action: action,
+           new_state: new_state,
+           reward: reward,
+        });
+    }
+
+    size(){
+        return this.count;
+    }
+
+    sample_batch(batch_size){
+        const batch = {
+            'states': [],
+            'actions': [],
+            'new_states': [],
+            'rewards': []
+        };
+        if(batch_size > this.count) {
+            console.warn("Batch size greater than buffer size, returning empty batch.");
+            return
+        }
+        for (let i = 0; i < batch_size/2; i++){
+
+        }
+    }
+}
