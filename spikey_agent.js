c58f25b6fa47574bf7355c3045c310b0231af2f9
Aaron Berdy <aaronberdy@ucla.edu>
2019-03-06 18:49:33 -0800

    quaternion invariant suborientation

--- spikey_agent.js
+++ spikey_agent.js
@@ -75,6 +75,8 @@ class RL_Agent extends Spikey_Agent {
             [10, 4, 9, 11, 5, 6],
             [11, 3, 7, 5, 10, 9]
         );
+
+        this.backwards = [1, 2, 8, 10, 11];
     }
 
     get_actuation(state, intent) {
@@ -92,7 +94,9 @@ class RL_Agent extends Spikey_Agent {
         */
         var t = state.t;
 
-        this.symmetrify_state(state, intent, 11);
+        for (var i of [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])
+            this.symmetrify_state(state, intent, i);
+        
         return [0, 0, 0, 0,
                 0, 0, 0, 0,
                 0, 0, 0, 0];
@@ -104,32 +108,81 @@ class RL_Agent extends Spikey_Agent {
     }
 
     symmetrify_state(state, intent, spike_ix) {
+//         console.log(state, intent, spike_ix);
         var spikey_orientation = state.orientation;
 
         var spike_0_q = spikey_orientation.times(this.subshapes[spike_ix].q).normalized(),
             spike_0_q_inv = spike_0_q.inverse();
 
-        var q_transform_0 = spike_0_q_inv;
+        var q_transform = Quaternion.of(0.7071, 0.7071, 0, 0).normalized().times(spike_0_q_inv);
+//         var q_transform = Quaternion.unit().times(spike_0_q_inv);
+
 
         var neighborhood = this.neighborhoods[spike_ix],
-            orientations = neighborhood.map(i => spikey_orientation.times(this.subshapes[i].q).normalized());
+            orientations = neighborhood.map(i => spikey_orientation.times(this.subshapes[i].q).normalized()),
+            filtered_subshapes = neighborhood.map(i => this.subshapes[i]);
+
+        var R_transform = Mat4.quaternion_rotation(q_transform);
+
+        var impulses = neighborhood.map(i => state.spikes[i].impulse),
+            relative_impulses = impulses.map((impulse, i) => 
+                Mat4.quaternion_rotation(orientations[i]).times(impulse));
+//         console.log(relative_impulses);
+
+        var transformed_1 = Mat4.quaternion_rotation(q_transform.times(orientations[1]).normalized()).times(
+            Vec.of(0, 0, 1)).to3(),
+            flat_1 = transformed_1.minus(transformed_1.project_onto(Vec.of(0, 1, 0))).normalized(),
+            theta = Math.atan(flat_1[0]/flat_1[2]) + (flat_1[2] < 0 ? PI : 0),
+//             normal = flat_1.cross(Vec.of(0, 0, 1)),
+
+//             theta = Math.asin(normal.dot(Vec.of(0, 1, 0))) + (this.backwards.includes(spike_ix) ? PI/5 : 0),
+            q_flat_transform = Quaternion.from(Math.cos(theta/2), Vec.of(0, 1, 0).times(Math.sin(-theta/2)));
+
+            console.log(10*theta/PI);
+//             transformed_intent = Mat4.quaternion_rotation(q_transform_0.times())
+
+//         console.log(Mat4.quaternion_rotation(q_transform.times(orientations[1]).normalized()))
+//         console.log(filtered_subshapes[1].shape.h_axis);
+//         console.log(transformed_1, flat_1);
+
+//         console.log(flat_1);
+
+        var R = Mat4.quaternion_rotation(spikey_orientation);
+
+        state.scene.shapes.vector.draw(
+            state.scene.globals.graphics_state,
+            Mat4.y_to_vec(flat_1.times(10000), Vec.of(0, 20, 20)),
+            state.scene.physics_shader.material(Color.of(1, 0, 0, 1)),
+            "LINES");
+            
 
 //         var s0_aligned_neighborhood = neighborhood.map(i => )
-        console.log(neighborhood);
         
 //         console.log(spike_0_q);
+    
 
-        var filtered_subshapes = neighborhood.map(i => this.subshapes[i]);
         
-        for (var i in orientations)
+        for (var i in orientations.slice(0, 2))
             state.scene.shapes.cone.draw(
                 state.scene.globals.graphics_state,
-                Mat4.translation(Vec.of(20, 20, 20).plus(filtered_subshapes[i].shape.com)).times(
-                Mat4.quaternion_rotation(orientations[i])).times(
+                Mat4.translation(Vec.of(0, 20, 20)).times(//).plus(
+//                     R.times(this.filtered_subshapes[i].d).minus(filtered_subshapes[i].shape.R.times(filtered_subshapes[i].shape.d)))).times(
+                Mat4.quaternion_rotation(q_flat_transform.times(q_transform.times(orientations[i])).normalized())).times(
+                Mat4.translation(filtered_subshapes[i].shape.d)).times(
                 Mat4.scale(Vec.of(2, 2, 20))),
                 state.scene.shader_mats.floor
                 );
-    }
 
+//         for (var i in orientations)
+//             state.scene.shapes.cone.draw(
+//                 state.scene.globals.graphics_state,
+//                 Mat4.translation(Vec.of(20, 20, 20)).times(//).plus(
+// //                     R.times(this.filtered_subshapes[i].d).minus(filtered_subshapes[i].shape.R.times(filtered_subshapes[i].shape.d)))).times(
+//                 Mat4.quaternion_rotation(q_transform.times(orientations[i]).normalized())).times(
+//                 Mat4.translation(filtered_subshapes[i].shape.d)).times(
+//                 Mat4.scale(Vec.of(2, 2, 20))),
+//                 state.scene.shader_mats.floor
+//                 );
+    }
 
 }
