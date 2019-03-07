065cc5cf9b1ae3a2ef146e7b9d55c2392ab1bb5e
Aaron Berdy <aaronberdy@ucla.edu>
2019-03-07 15:13:32 -0800

    reset spikey-pos option

--- training-scene.js
+++ training-scene.js
@@ -1,28 +1,32 @@
 class Training_Scene {
     constructor() {
-      this.shapes = {
-        'square': new Square(),
-        'mysquare': new MySquare(Color.of(0, 0, 1, 1)),
-        "linesegement": new LineSegment,
-        "vector": new Vector,
-        'circle': new Circle(15),
-        'pyramid': new Tetrahedron(false),
-        'simplebox': new SimpleCube(),
-        'box': new Cube(),
-        'cylinder': new Cylinder(15),
-        'cone': new Closed_Cone(20),
-        'ball': new Subdivision_Sphere(4),
+        this.shapes = {
+          'square': new Square(),
+          'mysquare': new MySquare(Color.of(0, 0, 1, 1)),
+          "linesegement": new LineSegment,
+          "vector": new Vector,
+          'circle': new Circle(15),
+          'pyramid': new Tetrahedron(false),
+          'simplebox': new SimpleCube(),
+          'box': new Cube(),
+          'cylinder': new Cylinder(15),
+          'cone': new Closed_Cone(20),
+          'ball': new Subdivision_Sphere(4),
 
-        'spikey': new Spikey_Shape(spikey_consts)
-      };
+          'spikey': new Spikey_Shape(spikey_consts)
+        };
+        this.spikey_starting_pos = Vec.of(0, spikey_consts.sphere_radius + spikey_consts.max_spike_protrusion, 0);
         var floor_material = Material.of(.35, .1, .01);
+        this.spikey_starting_pos = 
         this.shader_mats = {spikey: null};
-        this.Spikey = Spikey_Object.of(this, Vec.of(0, 0, 0), Vec.of(0, 0, 0), Vec.of(0, 0, 0), Quaternion.unit(), RL_AGENT);
-        this.floor = Box.of(this, Vec.of(0, -50, 0), Vec.of(0, 0, 0), Vec.of(0, 0, 0), Quaternion.unit(), Infinity, Vec.of(3000, 100, 5000), floor_material);
+        this.Spikey = Spikey_Object.of(this, this.spikey_starting_pos, Vec.of(0, 0, 0), Vec.of(0, 0, 0), Quaternion.unit(), RL_AGENT);
+        this.floor = Box.of(this, Vec.of(0, -50, 0), Vec.of(0, 0, 0), Vec.of(0, 0, 0), Quaternion.unit(), Infinity, Vec.of(100000, 100, 100000), floor_material);
         this.entities = [this.Spikey, this.floor];
     }
 
-    run_simulation(num_steps, dt, actuation, intent) {
+    run_simulation(num_steps, dt, actuation, intent, reset) {
+        if (reset)
+            this.reset_spiky_pos();
         this.give_intent(intent);
         this.give_actuation(actuation);
 
@@ -82,7 +86,11 @@ class Training_Scene {
 
     get_random_intent() {
         const scale = 5;
-        var random_intent = Vec.of(Math.random(), Math.random(), Math.random()).times(scale);
+        var random_intent = Vec.of(Math.random(), 0, Math.random()).times(scale);
         this.give_intent(random_intent);
+    }
+
+    reset_spiky_pos() {
+        this.Spikey.com = this.spikey_starting_pos;
     }
 }
